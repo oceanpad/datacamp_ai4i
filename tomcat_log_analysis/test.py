4947929d38a567e4fbd2526d64ef0e6143d5bf00
zhaohaiyang <zhy20090912@gmail.com>
2018-12-06 15:33:36 +0800

    update

--- tomcat_log_analysis/test.py
+++ tomcat_log_analysis/test.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import csv
 import re
 import glob
+import seaborn as sns
 
 csv_files = glob.glob('*.log')
 csv_str = ''
@@ -22,19 +23,46 @@ df = pd.read_csv('test.csv', delimiter=',', header = None)
 df.columns = ['ram', 'ts']
 df.ts=pd.to_datetime(df.ts)
 
-print df.head()
-print df.describe()
-print df.info()
+new_df = df.set_index('ts')
+
+#print new_df.head()
+#print new_df.describe()
+#print new_df.info()
+#print new_df.index
+
 
 df.to_csv('df_csv.csv')
 
-df.plot(x = 'ts', y = 'ram')
-ram_max = df['ram'].max()
-ts_max = df['ts'][df['ram'].argmax()]
+#new_df.plot()
 plt.xlable = 'ram'
 plt.ylable = 'ts'
-# Add a black arrow annotation
-plt.annotate('Max', xy=(ts_max, ram_max), xytext=(ts_max, ram_max+2),arrowprops=dict(facecolor='black'))
-plt.show()
-plt.savefig('tomcat.png')
+#print new_df.head()
+#print new_df.keys()
+plt.subplot(2,2,1)
+plt.plot(new_df)
+plt.legend(['all'])
 
+resampled_df = new_df.resample('50T').mean()
+plt.subplot(2,2,2)
+plt.plot(resampled_df, label='50mins mean')
+plt.legend()
+
+resampled_df2 = new_df.resample('50T').std()
+plt.subplot(2,2,3)
+plt.plot(resampled_df2, label='50mins std')
+plt.legend()
+
+print resampled_df
+
+#df.plot(x = 'ts', y = 'ram')
+#ram_max = df['ram'].max()
+#ts_max = df['ts'][df['ram'].argmax()]
+# Add a black arrow annotation
+#plt.annotate('Max', xy=(ts_max, ram_max), xytext=(ts_max, ram_max+2),arrowprops=dict(facecolor='black'))
+
+
+#sns.pairplot(df, kind='reg')
+
+fig1 = plt.gcf()
+plt.show()
+fig1.savefig('tomcat.png', dpi=300)
